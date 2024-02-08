12708a65f0202187f3083faef158eff8a1e8ae99
Sumit4509 <104025799+Sumit4509@users.noreply.github.com>
2024-02-09 00:02:19 +0530

    Time: 0 ms (100.00%), Space: 23.1 MB (29.31%) - LeetHub

---
+++ 2798-number-of-employees-who-met-the-target/2798-number-of-employees-who-met-the-target.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int numberOfEmployeesWhoMetTarget(vector<int>& hours, int target) {
+       int count =0;
+       for(int i=0 ;i< hours.size();i++){
+           if(hours[i]>=target){
+               count++;
+           }
+
+       } 
+       return  count;
+    }
+};
