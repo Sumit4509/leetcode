88c8e27c43ad360bc9053d34fc98d3940ceee31f
Sumit4509 <104025799+Sumit4509@users.noreply.github.com>
2024-06-10 23:55:39 +0530

    Time: 2 ms (88.93%), Space: 41 MB (80.38%) - LeetHub

---
+++ 1137-height-checker/1137-height-checker.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int heightChecker(int[] heights) {
+        int[]expected=heights.clone();
+        Arrays.sort(expected);
+        int ans=0;
+        for(int i=0;i<heights.length;i++){
+            if(heights[i]!=expected[i]){
+                ans=ans+1;
+            }
+        }
+        return ans;
+    }
+}
